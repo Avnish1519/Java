0c1fb767de81cca783dd440d2a70cb5d4e2a2efb
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-01-09 17:35:28 +0000

    initial changes

---
+++ JAVA/InsertionSort.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // Insertion sort
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > current) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = current;
+        }
+
+        System.out.print("Sorted Array: ");
+        printArray(arr);
+    }
+}
