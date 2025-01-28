d20449c7763f7f4c2ba4f57858a3d1a02d5b8f3c
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-01-28 14:27:11 +0000

    SinglyLinkedList

---
+++ LinkedList/SinglyLinkedlist.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+class LL {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head = null;
+    Node tail = null;
+
+    void insertbeg(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    void insertLast(int data) {
+        Node newNode = new Node(data);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class SinglyLinkedlist {
+    public static void main(String[] args) {
+        LL linkedList = new LL();
+        linkedList.insertbeg(10);
+        linkedList.insertLast(20);
+        linkedList.insertbeg(30);
+        linkedList.printList();
+    }
+}
