e214b452e178326865dc5ad2a06272d3e101a164
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-01-30 10:53:28 +0000

    Stack using Linked list

---
+++ JAVA/StackUsingLinkedList/Stack.java
@@ -0,0 +1,82 @@
+class SLL{
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+
+        }
+    }
+    Node head= null;
+
+    boolean isEmpty(){
+        return (head==null);
+    }
+
+    void push(int data){
+        Node newNode = new Node(data);
+        if(isEmpty()){
+            head  = newNode;
+        }
+        else{
+            newNode.next = head;
+            head=newNode;
+        }
+    }
+
+    int pop(){
+        
+        if(isEmpty()){
+            return -1;
+        }
+        else{
+            int top = head.data;
+            System.out.println(top+" deleted");
+            head=head.next;
+            return top;
+        }
+    }
+
+    void peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+        
+        } else {
+            System.out.println(head.data+" <- head");
+        }
+    }
+
+    void printList(){
+        if(isEmpty()){
+            System.out.println("List is Empty");
+            return;
+        }
+        else{
+            Node current = head;
+            while(current != null){
+                
+                System.out.print(current.data + " -> ");
+                current  = current.next;
+            }
+            System.out.println("null");
+
+        }
+    }
+}
+class Stack{
+    public static void main(String [] args){
+        SLL List = new SLL();
+
+        List.push(10);
+        List.push(20);
+        List.push(30);
+        List.pop();
+        List.peek();
+
+        List.printList();
+
+
+    }
+}
