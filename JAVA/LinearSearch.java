4656d0ea70363c3043cc8aabc3ea952c531085c4
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-03-20 14:09:09 +0000

    user defined input

--- JAVA/LinearSearch.java
+++ JAVA/LinearSearch.java
@@ -1,28 +1,36 @@
-class LinearSearch {
+import java.util.Scanner;
 
-      static int search(int a[], int n, int x)
-    {
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array: ");
         for (int i = 0; i < n; i++) {
-            if (a[i] == x)
-                return i;
+            arr[i] = sc.nextInt();
         }
 
-        // return -1 if the element is not found
-        return -1;
+        System.out.print("Enter the element to find: ");
+        int key = sc.nextInt();
+
+        int index = linearSearch(arr, key);
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found in the array");
+        }
     }
 
-    public static void main(String[] args)
-    {
-        int[] a = { 3, 4, 1, 7, 5 };
-        int n = a.length;
-        
-        int x = 4;
-
-        int index = search(a, n, x);
-        
-          if (index == -1)
-            System.out.println("Element is not present in the array");
-        else
-            System.out.println("Element found at position " + index);
+    
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i; 
+            }
+        }
+        return -1; 
     }
 }
