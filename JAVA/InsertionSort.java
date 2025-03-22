1538ab8cc1776ed9ef7b15428d1b5ba69ab0b107
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-03-22 17:34:28 +0000

    comment

--- JAVA/InsertionSort.java
+++ JAVA/InsertionSort.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class InsertionSort {
 
-    public static void printArray(int arr[]) {
+    public static void printArray(int arr[]) {//function
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
