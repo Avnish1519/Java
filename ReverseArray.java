5870063ce3a06c03450cf6c06555951dbbf88c1f
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-03-26 10:15:55 +0000

    ReverseArray

---
+++ ReverseArray.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class main{
+    public static void main(String [] args){
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextIn();
+        int [] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = new int[n];
+
+        }
+
+        int left = 0,right = arr.length-1;
+        while(left< right){
+            
+        }
+
+    }
+}
