2afb996afb08281cfad12b87ec8ffef986654756
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-04-03 16:10:13 +0000

    prefixSum

---
+++ Arrays/prefixSum.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6};
+        int n = arr.length;
+        int prefixSum[] = new int[n];
+        
+        
+        prefixSum[0] = arr[0]; 
+        for (int i = 1; i < n; i++) { 
+            prefixSum[i] = prefixSum[i - 1] + arr[i]; 
+        }
+        
+        System.out.println(Arrays.toString(prefixSum));
+    }
+}
