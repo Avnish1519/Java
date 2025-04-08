dc99bb30797951c7ca9d7f8c4ff0f0c9e53263b5
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-04-08 17:37:57 +0000

    comments

--- ReverseArray.java
+++ ReverseArray.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class main{
     public static void main(String [] args){//main method
         Scanner sc =  new Scanner(System.in);
-        int n = sc.nextIn();
+        int n = sc.nextIn();//size
         int [] arr = new int[n];//array declared
         for(int i=0; i<n; i++){//loop
             arr[i] = new int[n];
