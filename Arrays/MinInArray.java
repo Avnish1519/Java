d642e6dac9d05ddce989347485f8898cc81dc33a
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-04-29 10:58:16 +0000

    Comments

--- Arrays/MinInArray.java
+++ Arrays/MinInArray.java
@@ -6,10 +6,12 @@ public class Main
 		Scanner sc = new Scanner(System.in);
 		int n = 5;
 		int arr[] = new int[n];
+		//using for loop here
 		for(int i=0;i<n;i++){
 		    arr[i]=sc.nextInt();
 		}
 		int min=arr[0];
+		//again using a for loop for calculations
 		for(int i=1;i<n;i++){
 		    if(arr[i]<min){
 		        min=arr[i];
