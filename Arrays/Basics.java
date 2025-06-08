17e02bd1546ea2c333a5eed2e0654788d81a2a12
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-06-08 03:06:36 +0000

    sum of array

--- Arrays/Basics.java
+++ Arrays/Basics.java
@@ -4,7 +4,16 @@ class Basics{
     public static void main(String[] args){
         //  This is an object
         Scanner sc = new Scanner(System.in);
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 2, 3, 4, 5};
 
+        int sum = 0;
+
+        for (int i = 0; i < arr1.length; i++) {
+            sum += arr1[i] + arr2[i];
+        }
+
+        System.out.println("Sum: " + sum);
         int n = sc.nextInt();
         int arr[] = new int[n];  // Initializing the array with size n
 
