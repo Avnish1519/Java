3237e0189da5c18e69125c78ba7dfc84e01edb2b
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-06-12 16:31:27 +0000

    comments

--- ReverseArrayShifting.java
+++ ReverseArrayShifting.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class ReverseArrayShifting {
+public class ReverseArrayShifting {//performing reverse array shifting
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
