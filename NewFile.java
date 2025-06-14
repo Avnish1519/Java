9f120c6ace69fd51ed54c700d2d200052ccc1a4b
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-06-14 15:49:17 +0000

    comments

--- NewFile.java
+++ NewFile.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 class FileEx1{
     public static void main(String[] args) throws IOException {
-        FileWriter fr = new FileWriter("Test1.txt");
+        FileWriter fr = new FileWriter("Test1.txt");//cretes new file named as Test1.txt
         fr.write("This is an example file to create and write in new and existing file");
         fr.close();
     }
