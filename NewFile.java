cee08e66062ec0bd610f3a4ae185df4e564a1148
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-06-15 04:50:43 +0000

    File created

--- NewFile.java
+++ NewFile.java
@@ -1,9 +1,18 @@
 import java.io.*;
+import java.util.*;
 
 class FileEx1{
     public static void main(String[] args) throws IOException {
         FileWriter fr = new FileWriter("Test1.txt");//cretes new file named as Test1.txt
         fr.write("This is an example file to create and write in new and existing file");
         fr.close();
+
+        File f = new File("Test2.txt");
+        if(f.createNewfile) {
+            System.out.println("File created");
+        } else {
+            System.out.println("File not created");
+        }
     }
 }
+
