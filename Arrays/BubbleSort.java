af30cc8b38f5b3dc51f704a340bc28d1f8bb670b
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-08-07 21:57:28 +0530

    Update BubbleSort.java

--- Arrays/BubbleSort.java
+++ Arrays/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
         
         System.out.println("Sorted array:");
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+            System.out.print(array[i] + " ");//printing the array
         }
     }
     
