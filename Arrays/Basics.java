170c7127c9e062bd73a13bcf9dc4293862fc1c61
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-09-08 23:10:40 +0530

    Update Basics.java

--- Arrays/Basics.java
+++ Arrays/Basics.java
@@ -55,7 +55,7 @@ class Basics{
 
         int[][] num = {{1,2},{3,4}};
         
-        for(int i=0; i<num.length; i++) {
+        for(int i=0; i<num.length; i++) {//using for loop
             for(int j=0; j<num[i].length; j++) {
                 System.out.print(num[i][j]+" ");//printing output
             }
