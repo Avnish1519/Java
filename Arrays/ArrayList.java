a814ecf9082f54098ee5f666f4a1d435d54fc657
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-10-08 13:02:08 +0530

    Create ArrayList.java

---
+++ Arrays/ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        ArrayList<String> li = new ArrayList<>();
+        li.add("l");
+        
+        System.out.println(li);
+        String s = li.get(0);
+        System.out.println(s);
+        
+    }
+}
