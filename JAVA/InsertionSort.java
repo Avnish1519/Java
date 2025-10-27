d3c8283872aa0bd0a4bad33efd202c1d67150515
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-10-27 20:13:32 +0530

    Update InsertionSort.java

--- JAVA/InsertionSort.java
+++ JAVA/InsertionSort.java
@@ -28,7 +28,7 @@ public class InsertionSort {
             int current = arr[i];
             int j = i - 1;
 
-            while (j >= 0 && arr[j] > current) {
+            while (j >= 0 && arr[j] > current) {//using while loop
                 arr[j + 1] = arr[j];
                 j--;
             }
