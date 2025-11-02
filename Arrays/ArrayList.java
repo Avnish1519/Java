d94c371db512cc9d8ece65dc79c0477ba2391c29
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-11-02 23:10:03 +0530

    Update ArrayList.java

--- Arrays/ArrayList.java
+++ Arrays/ArrayList.java
@@ -4,6 +4,7 @@ class Main {
     public static void main(String[] args) {
         ArrayList<String> li = new ArrayList<>();
         li.add("l");//to add anything in the arraylist we use ".add"
+        li.remove(0);
         
         System.out.println(li);//printing the output
         String s = li.get(0);
