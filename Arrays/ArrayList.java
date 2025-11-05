19424fe957c9e3fbfa91e9a70c01474189ce87a7
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-11-05 19:38:56 +0530

    Add integer ArrayList and print its contents
    Added a new ArrayList of integers and printed it.

--- Arrays/ArrayList.java
+++ Arrays/ArrayList.java
@@ -9,6 +9,11 @@ class Main {
         System.out.println(li);//printing the output
         String s = li.get(0);
         System.out.println(s);//printing the output
+
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(1);
+        l1.add(2);
+        System.out.println(l1);
         
     }
 }
