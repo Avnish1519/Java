d12261955eefa235568a6633c0a21bc9603487d6
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-11-11 21:26:12 +0530

    comments

--- Arrays/ArrayList.java
+++ Arrays/ArrayList.java
@@ -10,7 +10,7 @@ class Main {
         String s = li.get(0);
         System.out.println(s);//printing the output
 
-        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l1 = new ArrayList<>();//using list interface
         l1.add(1);
         l1.add(2);
         System.out.println(l1);//printing the output
